a23451ba0ddf3f1c743a9bd91a33ee3c27740b84
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-31 15:06:57 +0100

    STOPCLOCK

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -8,12 +8,14 @@ namespace Tempo
     {
         static void Main(string[] args)
         {
+        
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
-            
-            crono1.Start;
+
+            crono1.Start();
             Thread.Sleep(500);
-            crono2.Start;
+
+            crono2.Start();
             Thread.Sleep(300);
 
             crono1.Stop();
@@ -22,11 +24,11 @@ namespace Tempo
             TimeSpan ts1 = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
 
-            string elapsedTime1 = String.Format("{2:00}", ts1.Seconds);
-            Console.WriteLine($"Cronómetro 1:{elapsedTime1}");
-            
-            string elapsedTime2 = String.Format("{2:00}", ts2.Seconds);
-            Console.WriteLine($"Cronómetro 2:{elapsedTime2}");
+            string elapsedTime1 = String.Format("{0:F2}", ts1.TotalSeconds);
+            Console.WriteLine($"Cronometro 1: {elapsedTime1}");
+
+            string elapsedTime2 = String.Format("{0:F2}", ts2.TotalSeconds);
+            Console.WriteLine($"Cronometro 2: {elapsedTime2}");
         }
     }
 }
