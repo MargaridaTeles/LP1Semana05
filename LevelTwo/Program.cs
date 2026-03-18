957989c7ccb45b8152518b119090631dccbe5e94
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-18 16:51:00 +0000

    Add tux image with lfs

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                CanvasImage image = new CanvasImage("tux.jpg");
+
+                image.MaxWidth(20);
+
+                AnsiConsole.Write(image);
+            }
+        }
+    }
+}
