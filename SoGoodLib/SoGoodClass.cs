5e0240bae6be2261838385288e770df9e431fe33
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-18 17:03:40 +0000

    Add new method to return a string

--- SoGoodLib/SoGoodClass.cs
+++ SoGoodLib/SoGoodClass.cs
@@ -5,5 +5,10 @@ namespace SoGoodLib
     public class SoGoodClass
     {
 
+        public static string SoGoodMethod()
+        {
+            return "I came from another project";
+        }
     }
+
 }
