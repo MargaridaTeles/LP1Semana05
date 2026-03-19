0b3430ee429541b9cee8267ea7bc29a71ef19f14
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-19 16:14:48 +0000

    Add new methods for Damage and CriticalHit

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -2,6 +2,7 @@
 
 namespace DungeonStats
 {
+
     public class Program
     {
         private static void Main(string[] args)
@@ -12,6 +13,25 @@ namespace DungeonStats
         private static int Damage(int attack, int defense)
         {
             return attack - defense;
+        }
+
+        private static int Damage(int attack)
+        {
+            // Devolve dano sem defesa
+            return attack;
+        }
+
+        private static int CriticalHit(int damage)
+        {
+            // devolve a soma de 1 até Damage(attack, defense) se damage <= 0 e devolve 0
+            int dano;
+
+            if (damage <= 0)
+                dano = 0;
+            else
+                dano = 1 + Damage(damage, 0);
+
+            return dano;
         }
         
     }
